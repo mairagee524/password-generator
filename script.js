6056402a5b05dc1801398b2e7b7e47bef2dc25c9
mgarcia24 <garciamaira524@yahoo.com>
2020-04-19 00:00:34 -0700

    finalized edits by adding functionality and style

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
-// 03 JavaScript: Password Generator
-// Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-// If you are unfamiliar with special characters, take a look at some examples.
-
-// User Story
-// AS AN employee with access to sensitive data
-// I WANT to randomly generate a password that meets certain criteria
-// SO THAT I can create a strong password that provides greater security
-
-// Acceptance Criteria
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
+// Setting variables with values
 var lowercase = "abcdefghijklmnopqrstuvwxyz".split('');
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 var number = "0123456789".split('');
 var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split('');
 var result = document.querySelector("#password");
 
+// Setting global variables
 var confirmLowercase,
   confirmUppercase,
   confirmNumbers,
   confirmSpecialCharacters,
   passwordLength;
 
-document.getElementById("generate").addEventListener("click",         function () {
+// Generate password by button click
+document.getElementById("generate").addEventListener("click",         
+function generatePassword() {
       alert("Thank you for using our generator. Please answer the following questions to customize your password.");
       passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+
+    // Setting condition for password length
+    if (passwordLength > 7 && passwordLength < 128) {
+      
+      // Establishing criteria for customized password
       confirmLowercase = confirm("Would you like lowercase letters in your password?");
       confirmUppercase = confirm("Would you like uppercase letters in your password?");
       confirmNumbers = confirm("Would you like numbers in your password?");
       confirmSpecialCharacters = confirm("Would you like special characters in your password?");
 
+      //Giving the generator a moment to construct password
       alert("Please wait a moment...");
 
+      // Results from true statements selected by user (pushed by if-statement)
       var password = [];
 
+      // Series of if-statements from each criteria
       if (confirmLowercase) {
         for (var i = 0; i < passwordLength; i++) {
           password.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
@@ -72,14 +58,30 @@ document.getElementById("generate").addEventListener("click",         function (
         };
       };
 
-  var together = [];
+    // Data validation if user inputs number outside of stated criteria  
+    } else if (passwordLength <= 7 || passwordLength >= 128) { {
+      passwordLength = confirm("The number you selected is not between 8 and 128. Please try again."); 
+      };
+        if (passwordLength) {
+          generatePassword();
+        } else {
+          return;
+        };
+    };
+    
+    // Alert if user selects "no" to all password criteria
+    if (confirmLowercase === false && confirmUppercase === false && confirmNumbers === false && confirmSpecialCharacters === false) {
+      alert("Sorry, you did not select enough criteria generate the pasword.");
+    };
 
-  for (var i = 0; i < passwordLength; i++) {
-    (together.push(password[Math.floor(Math.random() * password.length)]))
-  }
+    // New result by concatenation of previous password and reduce to number stated by user
+    var together = [];
 
-  console.log(together);
+    // For-loop generating password with number inputted by user
+    for (var i = 0; i < passwordLength; i++) {
+      (together.push(password[Math.floor(Math.random() * password.length)]));
+    };
 
-result.textContent = together.join('');
-
+    // Eliminate punctuation of new password generated
+    result.textContent = together.join('');  
 });
